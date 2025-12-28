1ef4865f73005ee84d57dd6c18e88ad1f6bd74ed
Vagner Vitor de Oliveira Melo <140123652+Ovitu@users.noreply.github.com>
2025-12-27 21:40:41 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "app_name" {
+  default = "simple-api"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "desired_count" {
+  default = 2
+}
+
+# --- REDE (VPC) ---
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags                 = { Name = "${var.app_name}-vpc" }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "${var.app_name}-igw" }
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.app_name}-public-${count.index + 1}" }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags              = { Name = "${var.app_name}-private-${count.index + 1}" }
+}
+
+resource "aws_eip" "nat" {
+  count  = 2
+  domain = "vpc"
+}
